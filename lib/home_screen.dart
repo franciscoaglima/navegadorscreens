ec6129e4316ceface29fcd8b74424ede7c239adb
lfrancisco <franciscoaglima@gmail.com>
2021-05-13 16:08:21 -0300

    4o Etapa - Tela NavegadorScreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:navegadorscreens/second_screen.dart';
+import 'package:navegadorscreens/third_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,16 +14,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
+      
+      mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      ElevatedButton(
-        onPressed: () async {
-          Navigator.pushNamed(context,"terceiro 3",);  
-      },
-      child: Text("Tela 3"),
-      ),
-    Container(
-     color: Colors.blue,
+     // ElevatedButton(
+     //   onPressed: () async {
+      //    Navigator.pushNamed(context,"terceiro 1",);  
+   //   },
+   //   child: Text("Tela 1"),
+    //  ),
+    Container(     
+     padding: EdgeInsets.all(50.0),
+     //color: Colors.blue,
+      //decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color: Colors.amber),
       child: ElevatedButton(
           onPressed: () async {
             final resultadoPop = await Navigator.push(
@@ -33,9 +37,26 @@ class _HomeScreenState extends State<HomeScreen> {
                   texto = resultadoPop;
                 });
           }, 
-          child: Text("Go Screen 2"),
+          child: Text("Tela 2"),
           ),
          ),
+    Container(
+
+     color: Colors.blue,
+      //decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),color: Colors.amber),
+      child: ElevatedButton(
+          onPressed: () async {
+            final resultadoPop = await Navigator.push(
+              context,
+               MaterialPageRoute(builder: (context) => ThirdScreen("Third Screen")),
+               );
+                setState(() {
+                  texto = resultadoPop;
+                });
+          }, 
+          child: Text("Tela 3"),
+          ),
+         ),         
          Material(child: Text(texto)),
       ],
     );
--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SecondScreen extends StatelessWidget {
+class ThirdScreen extends StatelessWidget {
   final String titulo;
 
 
-  SecondScreen(this.titulo, {Key key}): super(key: key);
+  ThirdScreen(this.titulo, {Key key}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SafeArea(
         child: Center(
           child: Column(
-            
             mainAxisAlignment: MainAxisAlignment.end,
             children: [ 
                Container(
